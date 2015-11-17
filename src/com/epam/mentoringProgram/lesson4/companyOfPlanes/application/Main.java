43d436c5b257f3d4d5b3e81e290c87d590812f6f
denis_shatskiy <denis_shatskiy@epam.com>
2015-11-17 18:10:02 +0600

    Change class for company, main to optimize

--- src/com/epam/mentoringProgram/lesson4/companyOfPlanes/application/Main.java
+++ src/com/epam/mentoringProgram/lesson4/companyOfPlanes/application/Main.java
@@ -2,8 +2,9 @@ package com.epam.mentoringProgram.lesson4.companyOfPlanes.application;
 
 import com.epam.mentoringProgram.lesson4.companyOfPlanes.my_exception.MyException;
 import com.epam.mentoringProgram.lesson4.companyOfPlanes.subject.CompanyOfPlanes;
-import com.epam.mentoringProgram.lesson4.companyOfPlanes.core.ConsoleAndFileOutput;
+import com.epam.mentoringProgram.lesson4.companyOfPlanes.core.DataOutput;
 import com.epam.mentoringProgram.lesson4.companyOfPlanes.core.FileReader;
+import com.epam.mentoringProgram.lesson4.companyOfPlanes.subject.Plane;
 
 import java.io.IOException;
 
@@ -11,15 +12,29 @@ public class Main {
 
     public static void main(String[] args) throws IOException, MyException {
         FileReader fileReader = new FileReader();
-        ConsoleAndFileOutput consoleAndFileOutput = new ConsoleAndFileOutput();
+        DataOutput dataOutput = new DataOutput();
         CompanyOfPlanes companyOfPlanes = fileReader.inputFromFile();
 
-        consoleAndFileOutput.createFile();
+        dataOutput.createFile();
 
-        companyOfPlanes.printPlaneList();
-        companyOfPlanes.printTotalCapacity();
-        companyOfPlanes.printTotalCarrying();
-        companyOfPlanes.printSortPlane();
+
+        dataOutput.consoleAndFileOutput("\nList of planes in the Company : ");
+        dataOutput.consoleAndFileOutput(companyOfPlanes.toString());
+
+        dataOutput.consoleAndFileOutput("Total capacity : ");
+        companyOfPlanes.defineTotalCapacity();
+        dataOutput.consoleAndFileOutput(String.valueOf(companyOfPlanes.getTotalCapacity()));
+
+        dataOutput.consoleAndFileOutput("\nTotal carrying : ");
+        companyOfPlanes.defineTotalCarrying();
+        dataOutput.consoleAndFileOutput(String.valueOf(companyOfPlanes.getTotalCarrying()));
+
+        dataOutput.consoleAndFileOutput("\nSorted list of planes in the Company : ");
+        companyOfPlanes.sortPlaneFromCompany();
+        dataOutput.consoleAndFileOutput(companyOfPlanes.toString());
+
+        dataOutput.consoleAndFileOutput("\nSearch by parameter \"Distance\" : ");
         companyOfPlanes.searchByParameter();
+        dataOutput.consoleAndFileOutput(companyOfPlanes.toString());
     }
 }
--- src/com/epam/mentoringProgram/lesson4/companyOfPlanes/subject/CompanyOfPlanes.java
+++ src/com/epam/mentoringProgram/lesson4/companyOfPlanes/subject/CompanyOfPlanes.java
@@ -1,6 +1,5 @@
 package com.epam.mentoringProgram.lesson4.companyOfPlanes.subject;
 
-import com.epam.mentoringProgram.lesson4.companyOfPlanes.core.ConsoleAndFileOutput;
 import com.epam.mentoringProgram.lesson4.companyOfPlanes.core.DistanceComparator;
 import com.epam.mentoringProgram.lesson4.companyOfPlanes.core.FileReader;
 
@@ -11,75 +10,61 @@ import java.util.List;
 
 public class CompanyOfPlanes {
     private List<Plane> planes = new ArrayList<>();
+    private List<Plane> resultOfSearch = new ArrayList<>();
     private double totalCapacity;
     private double totalCarrying;
+
+    NumberFormat formatter = NumberFormat.getNumberInstance();
+
+
+    public String toString(List<Plane> tempList){
+        String string = "";
+        for (Plane plane : tempList)
+            string += (plane.getName() + " - carrying: " + String.valueOf(formatter.format(plane.getCarrying())) +
+                    " , capacity: " + String.valueOf(formatter.format(plane.getCapacity())) +
+                    " , distance: " + String.valueOf(formatter.format(plane.getDistance())) + "\n");
+        return string;
+    }
 
     public void addPlanesToCompanyList(Plane plane) {
         planes.add(plane);
     }
 
-    NumberFormat formatter = NumberFormat.getNumberInstance();
-    ConsoleAndFileOutput consoleAndFileOutput = new ConsoleAndFileOutput();
-
-    public void printPlaneList() {
-        consoleAndFileOutput.consoleAndFileWrite("List of planes in company : ");
-        if (planes.isEmpty()) {
-            consoleAndFileOutput.consoleAndFileWrite("List is Empty");
-        } else {
-            for (Plane plane : planes) {
-                consoleAndFileOutput.consoleAndFileWrite(plane.toString());
-            }
-        }
+    public void defineTotalCapacity(){
+        for (Plane plane: planes) totalCapacity += plane.getCapacity();
     }
 
-    public double defineTotalCapacity(){
-        for (Plane plane: planes) totalCapacity += plane.getCapacity();
+    public Double getTotalCapacity(){
         return totalCapacity;
     }
 
-    public void printTotalCapacity(){
-        defineTotalCapacity();
-        consoleAndFileOutput.consoleAndFileWrite("\nTotal capacity : " + formatter.format(totalCapacity).toString());
-    }
-
-    public double defineTotalCarrying(){
+    public void defineTotalCarrying(){
         for (Plane plane: planes) totalCarrying += plane.getCarrying();
-        return totalCarrying;
     }
 
-    public void printTotalCarrying(){
-        defineTotalCarrying();
-        consoleAndFileOutput.consoleAndFileWrite("\nTotal carrying : " + formatter.format(totalCarrying).toString());
+    public Double getTotalCarrying(){
+        return totalCarrying;
     }
 
     public void sortPlaneFromCompany() {
         Collections.sort(planes, new DistanceComparator());
     }
 
-    public void printSortPlane(){
-        sortPlaneFromCompany();
-        consoleAndFileOutput.consoleAndFileWrite("\nSorted list of planes : ");
-        for (Plane plane : planes) {
-            consoleAndFileOutput.consoleAndFileWrite(plane.getName() + " : " + formatter.format(plane.getDistance()).toString());
-        }
-    }
-
-    public void searchByParameter() {
+    public List<Plane> searchByParameter() {
         FileReader fileReader = new FileReader();
         double[] limitsFroSearch = fileReader.inputTopAndBottomLimits();
         try {
-            consoleAndFileOutput.consoleAndFileWrite("\nSearch by parameter \"Distance\" (" + formatter.format(limitsFroSearch[0]) + " , " + formatter.format(limitsFroSearch[1]) + ")"); //как избавиться от статических значений?
-            for (Plane plane : planes){
-                double distance = plane.getDistance();
-                if(distance >= limitsFroSearch[0] && distance <= limitsFroSearch[1]){
-                    consoleAndFileOutput.consoleAndFileWrite(plane.getName() + " : " + formatter.format(plane.getDistance()));
+            //DataOutput.consoleAndFileOutput("\nSearch by parameter \"Distance\" (" + formatter.format(limitsFroSearch[0]) + " , " + formatter.format(limitsFroSearch[limitsFroSearch.length-1]) + ")");
+            for (Plane plane : planes) {
+                double parameterForSearch = plane.getDistance();
+                if (parameterForSearch >= limitsFroSearch[0] && parameterForSearch <= limitsFroSearch[1]) {
+                    planes.add(plane);
                 }
             }
-        }
-        catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("There was null value");
         }
-
+        return planes;
     }
 }
 
