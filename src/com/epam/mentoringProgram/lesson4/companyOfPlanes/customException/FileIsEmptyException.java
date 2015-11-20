627bae7ade1240792206c4a8e57b0f2076a95d84
denis_shatskiy <denis_shatskiy@epam.com>
2015-11-20 17:54:45 +0600

    Fix

--- src/com/epam/mentoringProgram/lesson4/companyOfPlanes/customException/FileIsEmptyException.java
+++
@@ -1,7 +0,0 @@
-package com.epam.mentoringProgram.lesson4.companyOfPlanes.customException;
-
-public class FileIsEmptyException extends Exception {
-    public FileIsEmptyException() {
-        super("File is empty!");
-    }
-}
--- src/com/epam/mentoringProgram/lesson4/companyOfPlanes/customException/NegativeValueException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.mentoringProgram.lesson4.companyOfPlanes.customException;
-
-public class NegativeValueException extends Exception {
-    public NegativeValueException() {
-        super("There was negative value!");
-    }
-}
-
