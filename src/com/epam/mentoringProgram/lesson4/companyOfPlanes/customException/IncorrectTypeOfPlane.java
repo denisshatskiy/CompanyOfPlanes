35ec674b147759ddc9897c39b135a50cb773b0ca
denis_shatskiy <denis_shatskiy@epam.com>
2015-11-20 17:54:52 +0600

    Fix

--- src/com/epam/mentoringProgram/lesson4/companyOfPlanes/customException/IncorrectTypeOfPlane.java
+++
@@ -1,7 +0,0 @@
-package com.epam.mentoringProgram.lesson4.companyOfPlanes.customException;
-
-public class IncorrectTypeOfPlane extends Exception {
-    public IncorrectTypeOfPlane() {
-        super("You entered an incorrect code!");
-    }
-}
